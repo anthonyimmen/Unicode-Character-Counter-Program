57417f35b63e7a54a7192b6f9bf9ef9f81e7b26f
anthonyimmen <anthonyimmen@ou.edu>
2021-10-18 09:58:16 +0000

    project sort of works? v1.0

--- project0.c
+++ project0.c
@@ -5,91 +5,200 @@
 #include <wchar.h>
 #include <locale.h>
 #include <limits.h>
+#include <stdbool.h>
 
-
+#define BIG_BOY 1112064 //that's one big boy
 
 struct  UTFchar {
 
-	int counter;
-	char bytes[4];
-	char *next[];
-	bool flag;
-	char originalhex;
+	long long counter;
+	unsigned char bytes1;
+	unsigned char bytes2;
+	unsigned char bytes3;
+	unsigned char bytes4;
+	int bytescount;
 
 }Unichar;
 
-struct list {
-
-	struct UTFchar Unichar;
-	struct list *next;
-
-}Node;
-
-//this function creates the linked list by initializing the head with init, so init now becomes the head
-void createList(Node *init, UTFchar *unichar) {
-	
-	Node **head = (Node**)malloc(sizeof(struct list));
-
-	init->Unichar = *unichar; 
-
+void sortinput(struct UTFchar *list, long long counter) { //this function will sort the current array indescending order
+	struct UTFchar *temp = list;
+	for (int i = 0; i < counter; i++) {
+		struct UTFchar *tempi = list+i;
+		for (int j = i + 1; j < counter; ++j) {
+			struct UTFchar *tempj = list+j;
+			if (tempi->counter < tempj->counter) { //does this include char of same count rightnext toeachother
+				temp = list+i;
+				tempi = (list+j);
+				tempj = temp;
+			}	
+		}
+	}
 }
 
-//this functions adds another node to our list
-void addUniChar(Node *current, UTFchar *unichar) {
 
-	current->next = *unichar;
+void printinput(struct UTFchar *list, long long counter) { //this function will print the output in the specifed format
+	struct UTFchar *tempi;
+	for (int i = 0; i < counter; ++i) {
+	tempi = list+i;
+	unsigned char unicodebytes[4];
+		unicodebytes[0] = tempi->bytes1;
+		unicodebytes[1] = tempi->bytes2;
+		unicodebytes[2] = tempi->bytes3;
+		unicodebytes[3] = tempi->bytes4;
+	unsigned char *unicodestr = &unicodebytes[0];
+		printf("%s->%llu\n", unicodestr, tempi->counter);
+	}
 
 }
 
 
 int program() {
-	
-	//create a structure linked list to connect the bytes together
-	Unichar *formstruct = (Unichar*)malloc((sizeof(struc UTFchar));
-	Node *init = (Node*)malloc((sizeof(struc list));
-	
 
-	int nums[100];
-	int i;
-
-	while ((scanf("%d", &nums[i]) == 1)) {
+	char tempbyte;	
+	unsigned char onebyte;
+	unsigned char twobyte;
+	unsigned char threebyte;
+	unsigned char fourbyte;
+	struct UTFchar *list;
+	long long counter = 1;
+	int bytecount; 
+	struct UTFchar *head = (struct UTFchar *)(malloc(BIG_BOY));
+	printf("you made it not really\n");
+	//create a dynamically sized array of Unichars using malloc
+	list = (struct UTFchar *)(malloc(BIG_BOY));
+	head = list;
+	//need to read in first character from file
+	tempbyte = fgetc(stdin); //read in first byte of the file
+	printf("past malloc, still not good\n");
+        //this loop will scan in the rest of the bytes associtated with the current character
+	while (tempbyte != EOF) {
+		onebyte = tempbyte;
+	if (onebyte >= 240) {
+		bytecount = 4;
+		twobyte = (unsigned char)fgetc(stdin);	
+		threebyte = (unsigned char)fgetc(stdin);
+		fourbyte = (unsigned char)fgetc(stdin);
+	}
+	else if (onebyte >= 224) {
+		bytecount = 3;
+		twobyte = (unsigned char)fgetc(stdin);
+		threebyte = (unsigned char)fgetc(stdin);
+	}
+	else if (onebyte >= 192) {
+		bytecount = 2;
+		twobyte = (unsigned char)fgetc(stdin);
+	}
+	else { //this is ASCII
+		bytecount = 1;
+	} 
+	printf("past first byte ig \n");
+	//need to scan through array and find where the current character is 
 	
-	i++;
+	int i = 0;
+	int flag = 0;
+	struct UTFchar *headcopy = (struct UTFchar *)(malloc(BIG_BOY));
+	headcopy = head;
+	printf("past init head");
+	while(i < counter) { //will scan through the array to see if a node already exists
+	//need to compare current character bytes with every UTFchar inside of array
+		if ((bytecount == 4) && (onebyte == headcopy->bytes1) && (twobyte == headcopy->bytes2) && 
+		(threebyte == headcopy->bytes3) && (fourbyte == headcopy->bytes4)) {
+		headcopy->bytes1 = onebyte;
+		headcopy->bytes2 = twobyte;
+		headcopy->bytes3 = threebyte;
+		headcopy->bytes4 = fourbyte;
+		headcopy->counter += 1;
+		headcopy->bytescount = 4;
+		flag = 1;
+		break;
+		printf("past 4 byte");
+		}
+	
+		else if ((bytecount == 3) && (onebyte == headcopy->bytes1) && (twobyte == headcopy->bytes2) && 
+		(threebyte == headcopy->bytes3)) {
+		headcopy->bytes1 = onebyte;
+		headcopy->bytes2 = twobyte;
+		headcopy->bytes3 = threebyte;
+		headcopy->counter += 1;
+		headcopy->bytescount = 3;
+		flag = 1;
+		break;
+		}
 
+		else if ((bytecount == 2) && (onebyte == headcopy->bytes1) && (twobyte == headcopy->bytes2)) {
+		headcopy->bytes1 = onebyte;
+		headcopy->bytes2 = twobyte;
+		headcopy->counter += 1;
+		headcopy->bytescount = 2;
+		flag = 1;
+		break;
+		}
+
+		else if ((bytecount == 1) && (onebyte == headcopy->bytes1)) {
+		headcopy->bytes1 = onebyte;
+		headcopy->counter += 1;
+		headcopy->bytescount = 1;
+		flag = 1;
+		break;
+		}
+		
+		else { //if none match then increment to the next index in array
+		++headcopy;
+		++i;
+		}	
+	}
+	printf("past 2nd while whooo\n");
+	if (flag == 0) { //flag will still be zero if the node hasnt been found, this will create a new node in array
+		struct UTFchar *temp = (struct UTFchar *)(malloc(BIG_BOY));
+		temp = headcopy+counter;
+		if (bytecount == 4) {
+		temp->bytes1 = onebyte;
+		temp->bytes2 = twobyte;
+		temp->bytes3 = threebyte;
+		temp->bytes4 = fourbyte;
+		temp->bytescount = 4;
+		temp->counter += 1;
+		}
+
+		else if (bytecount == 3) {
+		temp->bytes1 = onebyte;
+		temp->bytes2 = twobyte;
+		temp->bytes3 = threebyte;
+		temp->bytescount = 3;
+		temp->counter += 1;
+		}
+
+		else if (bytecount == 2) {
+		temp->bytes1 = onebyte;
+		temp->bytes2 = twobyte;
+		temp->bytescount = 2;
+		temp->counter += 1;
+		}
+	
+		else if (bytecount == 1) {
+		temp->bytes1 = onebyte;
+		temp->bytescount = 1;
+		temp->counter += 1;
+		}
+
+		else {
+		break; //this will cover the empty file case
+		}
+	}
+	printf("fully assigned first variable\n");
+	//	increment onebyte to the next byte in the input file
+		tempbyte = fgetc(stdin);
+		++counter;
+	printf("you made first round\n");
 	}
 
-
-	for (int j=0; j < i; j++) {
-		printf("%d\n", nums[j]);
-	}
-
-
-
-
-
-
-
-
-// this section will be used to output the characters to the screen
-
-
-	wchar_t string[100];
-	 setlocale(LC_ALL, "");
-
-	 printf ("Enter a string: ");
-	 scanf("%ls", string); 
-
-	 printf("String Entered: %ls: length: %d\n", string, wcslen(string));
-       	return 0;
+	//we have now read in all the characters now we need to sort and output
+	
+	sortinput(list, counter);
+	printinput(list, counter);
+	
+	return 0;
 }
-
-
-
-
-
-
-
-
 
 
 int main(int argc, char** argv)
